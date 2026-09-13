73911d633ddd4ff66d1966fe2b6aa950324cae81
Rica Caballero <128268961+cariknight@users.noreply.github.com>
2026-09-13 14:07:24 +0800

    ADDED FileService.cs

--- ereader/Services/FileService.cs
+++ ereader/Services/FileService.cs
@@ -1,12 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ereader.Services
+﻿namespace ereader.Services
 {
-    internal class FileService
+    public class FileService
     {
+        public async Task<string?> PickBookAsync()
+        {
+            var result = await FilePicker.Default.PickAsync(
+                new PickOptions
+                {
+                    PickerTitle = "Choose a book",
+                    FileTypes = new FilePickerFileType(
+                        new Dictionary<DevicePlatform, IEnumerable<string>>
+                        {
+                            {
+                                DevicePlatform.WinUI,
+                                new []
+                                {
+                                    ".epub",
+                                    ".pdf",
+                                    ".txt",
+                                }
+                            },
+                            {
+                                DevicePlatform.Android,
+                                new[]
+                                {
+                                    "application/epub+zip",
+                                    "application/pdf",
+                                    "text/plain"
+                                }
+                            }
+                        })
+                }
+                );
+            if (result == null) return null;
+
+            string extension = Path.GetExtension(result.FileName).ToLowerInvariant();
+            string destination = Path.Combine(FileSystem.AppDataDirectory, "Books");
+            Directory.CreateDirectory(destination);
+
+            string destinationPath = Path.Combine(destination, $"{Guid.NewGuid()}{extension}");
+
+            using Stream source = await result.OpenReadAsync();
+            using FileStream destinationStream = File.Create(destinationPath);
+
+            await source.CopyToAsync(destinationStream);
+            return destinationPath;
+        }
     }
 }
