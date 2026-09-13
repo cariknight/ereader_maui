143231db02b689e9060070b1001f637b8671ef36
Rica Caballero <128268961+cariknight@users.noreply.github.com>
2026-09-13 14:28:05 +0800

    UPDATED MauiProgram.cs

--- ereader/MauiProgram.cs
+++ ereader/MauiProgram.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
+using CommunityToolkit.Maui;
+using ereader.Services;
 
 namespace ereader
 {
@@ -9,12 +11,26 @@ namespace ereader
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
+            builder.Services.AddSingleton<DatabaseService>();
+            builder.Services.AddSingleton<FileService>();
+            builder.Services.AddSingleton<BookService>();
+
+            builder.Services.AddSingleton<Views.HomePage>();
+            builder.Services.AddSingleton<Views.LibraryPage>();
+            builder.Services.AddSingleton<Views.ReaderPage>();
+            builder.Services.AddSingleton<Views.SettingsPage>();
+
+            builder.Services.AddTransient<ViewModels.HomeViewModel>();
+            builder.Services.AddTransient<ViewModels.LibraryViewModel>();
+            builder.Services.AddTransient<ViewModels.ReaderViewModel>();
+            builder.Services.AddTransient<ViewModels.SettingsViewModel>();
 #if DEBUG
     		builder.Logging.AddDebug();
 #endif
